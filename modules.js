33b2e1f74ae4397cf88ae15c113b847a27c49d07
Modupe Ade-Onojobi <modupeadeonojobi@gmail.com>
2021-10-11 22:49:14 +0100

    Update modules and people files

--- modules.js
+++ modules.js
@@ -1,2 +1,17 @@
-const abc = require('./people');
+// const abc = require('./people');
 
+// console.log('The whole object', abc);
+
+// console.log('Singular', abc.people, abc.ages);
+
+
+//USING DESTRUCTING
+const { people, ages } = require('./people');
+
+console.log(people, ages);
+
+// USING BUILT IN MODULES
+
+const os = require('os');
+
+console.log(os.homedir(), os.platform());
